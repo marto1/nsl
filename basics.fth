74310865750b4c0b9ac3d367ef0868cfbd88c1fe
Martin Gergov <martingergov1@gmail.com>
2017-02-23 11:12:16 +0100

    some util functions and a basic help system for builtins added.

--- basics.fth
+++ basics.fth
@@ -3,3 +3,6 @@
 : >= 2dup > rot = or ;
 : <= 2dup < rot = or ;
 : xor 2dup and invert rot or and ;
+: True -1 ;
+: False 0 ;
+: 0> 0 > ;
--- picoforth.py
+++ picoforth.py
@@ -6,66 +6,81 @@ Should eventually replace the messy one in p2psim.
 import fileinput
 import sys
 
-
 # builtins
 
 def print_stack(stack, words):
+    """print stack ( -- )"""
     print stack
 
 def print_words(stack, words):
+    """print all defined words ( -- )"""
     print words.keys()
 
 def print_pop(stack, words):
+    """pop top and print it ( a -- )"""
     print stack.pop()
 
 def add(stack, words):
+    """add two numeric values ( n1 n2 -- n1+n2 )"""
     first = stack.pop()
     stack.append(stack.pop() + first)
 
 def dup(stack, words):
+    """duplicate top ( a -- a a )"""
     stack.append(stack[-1])
 
 def negate(stack, words):
+    """negate numeric value ( n -- -n )"""
     stack.append(-stack.pop())
 
 def logic_and(stack, words):
+    """logical and ( a b -- -1/0 )"""
     second = stack.pop() #prevent short circuit
     stack.append(stack.pop() and second)
 
 def logic_or(stack, words):
+    """logical or ( a b -- -1/0 )"""
     second = stack.pop() #prevent short circuit
     stack.append(stack.pop() or second)
 
 def bigger(stack, words):
+    """bigger than test ( a b -- -1/0 )"""
     second = stack.pop()
     stack.append(-1 if stack.pop() > second else 0)
 
 def smaller(stack, words):
+    """smaller than test ( a b -- -1/0 )"""
     second = stack.pop()
     stack.append(-1 if stack.pop() < second else 0)
 
 def equal(stack, words):
+    """equality test ( a b -- -1/0 )"""
     second = stack.pop()
     stack.append(-1 if second == stack.pop() else 0)
 
 def over(stack, words):
+    """copy 2nd element to top ( a b -- a b a )"""
     stack.append(stack[-2])
 
 def rotate(stack, words):
+    """place first element to 3rd place ( a b c -- c b a )"""
     el = stack.pop()
     stack.insert(-2, el)
 
 def swap(stack, words):
+    """swap 2 topmost elements ( a b -- b a )"""
     second = stack.pop()
     first = stack.pop()
     stack.append(second)
     stack.append(first)
 
 def invert(stack, words):
+    """invert numerical value ( 0 -- -1 ; -1 -- 0 )"""
     stack.append(-1 if stack.pop() == 0 else 0)
 
 
 def define_end(stack, words):
+    """end of definition."""
     definition = []
     while len(stack) > 0:
         el = stack.pop()
@@ -78,6 +93,9 @@ def define_end(stack, words):
             definition.append(str(el))
     return 0
 
+def func_help(stack, words):
+    """take 'word from stack and print help for word"""
+    print words[stack.pop()[1:]].func_doc
 
 words = {
     ".s": print_stack,
@@ -97,6 +115,7 @@ words = {
     ">": bigger,
     "<": smaller,
     "=": equal,
+    "help": func_help
 }
 # end builtins
 
