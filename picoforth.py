d76dc5e09b05eb02de2ae6ff0fd7c04366db418c
Martin Gergov <martingergov1@gmail.com>
2017-03-15 20:00:28 +0100

    more info on definitions

--- picoforth.py
+++ picoforth.py
@@ -11,6 +11,7 @@ stack 1 print                  print(stack)
    '  '-- number of items to pull from the stack
    ' check stack type, is variable so put on stack
 " Hello world! " 1 print       print("Hello World!")
+x : " Hello " 1 print ; def    def x(): print("Hello")
 
 """
 from __future__ import print_function
