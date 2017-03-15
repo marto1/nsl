7ff17f59ef6541ecf71005efd03a96967658ea4b
Martin Gergov <martingergov1@gmail.com>
2017-03-15 22:25:00 +0100

    definition docs

--- picoforth.py
+++ picoforth.py
@@ -10,9 +10,16 @@ stack 1 print                  print(stack)
    '  '  ' check print type, function so consume arguments and execute
    '  '-- number of items to pull from the stack
    ' check stack type, is variable so put on stack
-" Hello world! " 1 print       print("Hello World!")
-x : " Hello " 1 print ; def    def x(): print("Hello")
 
+" Hello world! " 1 print       print("Hello World!")
+
+x : " Hello " 1 print ; def    def x(): print("Hello")
+' '   '               '  '
+' '   '               '  '  define keyword
+' '   '               ' end definiton block
+' '   'definition block
+' 'begin definition block
+'definition name
 """
 from __future__ import print_function
 from time import time, sleep
